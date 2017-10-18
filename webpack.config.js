52c5b17a11059778f79837ed3749388fe85220ad
denier1025 <denier1025@gmail.com>
2017-10-18 21:24:35 +0300

    config for production & development

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const PATHS = {
 	build: path.join(__dirname, 'dist')
 };
 
-module.exports = {
+const common = {
   entry: {
       'index': PATHS.source + '/pages/index/index.js',
       'blog': PATHS.source + '/pages/blog/blog.js'
@@ -51,5 +51,21 @@ module.exports = {
         template: PATHS.source + '/pages/blog/blog.pug'
     }),
 	new ExtractTextWebpackPlugin('[name].css')
-  ]
+    ]
+};
+
+const developmentConfig = {
+    devServer: {
+        stats: 'errors-only',
+		port: 9000
+    }
+};
+
+module.exports = function(env) {
+	if(env === 'production') {
+		return common;
+	}
+	if(env === 'development') {
+		return Object.assign(common, developmentConfig);
+	}
 };
